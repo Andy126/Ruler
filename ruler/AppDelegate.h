dbb6a890ff7afa9f6c87ca95f19cd0b6c3982945
andy <andy@andydemacbook-pro.local>
2018-04-11 15:42:00 +0800

    Initial Commit

---
+++ ruler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ruler
+//
+//  Created by andy on 2018/4/11.
+//  Copyright © 2018年 andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ruler/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ruler
+//
+//  Created by andy on 2018/4/11.
+//  Copyright © 2018年 andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
